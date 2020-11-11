64f95b0cda916d38d0f9b887ef8661829e7a3cca
Esteban Ortiz Tirado Gonzalez <if708924@iteso.mx>
2020-11-10 19:18:39 -0600

    Daily (or other) frequency data aggrupation Function created in data.py and modified main.py to call such function

--- data.py
+++ data.py
@@ -10,6 +10,7 @@
 """
 import pandas as pd
 
+
 def f_leer_archivo(param_archivo: str):
     # print(param_archivo[-4:])
     if param_archivo[-3:] == 'csv':
@@ -21,3 +22,27 @@ def f_leer_archivo(param_archivo: str):
     else:
         data = "Ingresa un formato Excel válido"
         return data
+ 
+    
+def load_data(start: int = 2007, end: int = 2020, freq: str = 'D'):
+    column_names = ["TimeStamp", "open", "high", "low", "close", "volume"]
+    data = pd.DataFrame()
+    for year in range(start, end+1):
+        file = 'files/ME_'+str(year)+'.csv'
+        maindf  = pd.read_csv(file,
+                              header=1,
+                              names=column_names, 
+                              parse_dates=["TimeStamp"],
+                              index_col=["TimeStamp"] )
+        
+        sampled_df =  maindf.resample(freq).agg({'open': 'first', 
+                                                 'close': 'last', 
+                                                 'high' : 'max', 
+                                                 'low' : 'min', 
+                                                 'volume': 'sum'})
+        
+        sampled_df = sampled_df[sampled_df.open > 0]
+        if freq == 'D':
+            sampled_df.index = sampled_df.index.date
+        data = data.append(sampled_df)
+    return data
